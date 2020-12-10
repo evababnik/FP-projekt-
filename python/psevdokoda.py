9e0618ead352e58295fe53d803231289d9101dc5
zaloznikjan7 <zaloznikjan@yahoo.com>
2020-12-10 16:33:40 +0100

    Uredil skico kode

--- python/psevdokoda.py
+++ python/psevdokoda.py
@@ -1,40 +1,51 @@
-#Psevodokodi za solve_RKP in rekurzija
+Psevodokodi za solve_RKP in rekurzija
 
 def solve_RKP(N, c, w, p, lamda = None,  max_w = None):
-    # Najprej uredimo podatke po padajoči teži (max_w - w)
-    # Če je max_w = None, uredimo podatke padajoče po teži (w)
+    Najprej uredimo podatke po padajoči teži (max_w - w)
+    Če je max_w = None, uredimo podatke padajoče po teži (w)
 
-    # Naredimo matrike Z, K, G in nastavimo začetne pogoje
-    for d := 0 in range(c + 1): 
+    Naredimo matrike Z, K, G ter nastavimo začetne pogoje
+    Element v prvi vrstici in prvem stolpcu matrike nastavimo na 0 vse ostale na minus neskončno:
+    for d in range(c + 1): 
         for s in range(lamda + 1): 
             Z[d][s]= float("-inf")
-    Z[0][0] = 0  # Element v prvi vrstici in prvem stolpcu matrike nastavimo na 0 vse ostale na minus neskončno
-    # Analogno naredimo še matriki K in G
+    Z[0][0] = 0  
+    Analogno naredimo še matriki K in G
 
-    for j in range(len(N)): # izberemo j-ti predmet 
-        for d in range(c, w[j]-1, -1):  # in ga poskusimo dodati v nahrbtnik v svoji nominalni teži 
-            if z[d - w[j]][lamda] + p[j] > z[d][lamda]: #pri pogoju, da smo že vstavili 
-                z[d][lamda] = z[d - w[j]][lamda] + p[j]  #lamda predmetov
-                k[d][lamda] = 1 + k[d - w[j]][lamda]
+    izberemo j-ti predmet:
+    for j in range(len(N)): 
+        j-ti predmet poskusimo dodati v nahrbtnik v svoji nominalni teži pri pogoju, da smo že vstavili lamda predmetov
+        for d in range(c, w[j]-1, -1):  
+            if Z[d - w[j]][lamda] + p[j] > Z[d][lamda]:
+                Z[d][lamda] = Z[d - w[j]][lamda] + p[j] 
+                K[d][lamda] = 1 + K[d - w[j]][lamda]
                 if j  >= ((len(N) / 2)):
-                    g[d][lamda] = 1 + g[d - w[j]][lamda]
-                      
-        for s in range(lamda, 0, -1): # poskusimo ga dodati v svoji robustni teži
-            for d in range(c, maks_w[j] - 1, -1):
-                if z[d - maks_w[j]][s - 1] + p[j] > z[d][s]:
-                    z[d][s] = z[d - maks_w[j]][s - 1] + p[j]
-                    k[d][s] = 1 + k[d - maks_w[j]][s-1] 
-                    if j  >= ((len(N) / 2)):
-                        g[d][s] = 1 + g[d - maks_w[j]][s - 1]
+                    G[d][lamda] = 1 + G[d - w[j]][lamda]
 
-        z_zvedica = max([max(l) for l in z]) #tako dobimo max vrednost (največji element matrike z)
-        pozicija = [[index, vrstica.index(z_zvedica)] for index, vrstica in enumerate(z) if z_zvedica in vrstica]
-        c_zvezdica = pozicija[0][0] #vrstica in stolpec max vrednosti predstavljata skupno težo vstavljenih predmetov
-        stevilo_predmetov_s_povecano_tezo = pozicija[0][-1] #in število predmetov, ki se jim spremeni teža
-        g1 = g[c_zvezdica][stevilo_predmetov_s_povecano_tezo] #g1 se v g nahaja na istem mestu kot z_zvezdica(pomeni koliko elementov iz N2 je v optimalni rešitvi)
-        k_zvezdica = k[c_zvezdica][stevilo_predmetov_s_povecano_tezo] #na istem mestu kot z_zvezdica se nahaja tudi število vseh vstavljenih predmetov v optimalni rešitvi
-        g_zvezdica = k_zvezdica - g1 #g_zvezdica = št.elementov v N1, k_zvezdica = št. vseh elementov
-        return [z_zvedica, c_zvezdica, k_zvezdica, g_zvezdica]
+        j-ti predmet poskusimo dodati v svoji robustni teži:            
+        for s in range(lamda, 0, -1):
+            for d in range(c, maks_w[j] - 1, -1):
+                if Z[d - maks_w[j]][s - 1] + p[j] > Z[d][s]:
+                    Z[d][s] = Z[d - maks_w[j]][s - 1] + p[j]
+                    K[d][s] = 1 + K[d - maks_w[j]][s-1] 
+                    if j  >= ((len(N) / 2)):
+                        G[d][s] = 1 + G[d - maks_w[j]][s - 1]
+
+        Največji člen matrike Z je z*:
+        z* = max([max(i) for i in Z])
+
+        Pogledamo kje v matriki se nahaja z*:
+        pozicija = [[index, vrstica.index(z_zvedica)] for index, vrstica in enumerate(Z) if z_zvedica in vrstica]
+
+        vrstica ter stolpec maksimalne vrednosti predstavljata skupno težo vstavljenih predmetov:
+        c* = pozicija[0][0]
+        število_predmetov_s_povečano_težo = pozicija[0][-1] #to je število predmetov, ki se jim je spremenila teža
+
+        Najdemo k* ter g1, ki pomeni koliko elementov iz N2(druga polovica predmetov v N) je v optimalni rešitvi:
+        g1 = G[c*][število_predmetov_s_povečano_težo] 
+        k* = K[c*][število_predmetov_s_povečano_težo] 
+        g* = k* - g1
+        return [z*, c*, k*, g*]
 
 def rekurzija(N, z*, k*, c*, lamda, w, maks_w, p, vstavljeni_predmeti=[]):
     if len(N) == 1 and vstavljeni_predmeti == []:
